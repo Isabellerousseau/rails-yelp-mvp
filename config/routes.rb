ca47118487588d756a54f7e71f03f8b85e461b09
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-02-19 16:05:15 +0100

    Rails Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: %i[new create]
+  end
+  # resources :reviews, only: %i[show edit update destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
